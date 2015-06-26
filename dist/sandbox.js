4e228edb3399c32fd66b7346cb72520c5d5bb6a8
BuildTools <christian@decode.org.nz>
2015-06-27 09:38:41 +1200

    Use codemirror changelist ordering
    Fixes #2

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -309,27 +309,12 @@ Sandbox.prototype.bindRecoder = function (recoder) {
   var sandbox = this;
 
   // Stolen from the recode-brackets extension
-  // This function *tries* to handle multiple selections
-  // Newlines tend to break that
   cm.on("changes", function (cm, changelist) {
     if (recoder.recording) {
       var first = true;
 
-      // Make sure the changes are in reverse order
-      // So we don't have to worry about post-change
-      // Cordinates
-      if (changelist.length > 1) {
-        changelist = changelist.slice().sort(function (a, b) {
-          if ((a.to.line > b.from.line) || ((a.to.line == b.from.line) && (a.to.ch > b.from.ch))) {
-            return -1;
-          } else if ((a.to.line == b.from.line) && (a.to.ch == b.from.ch)) {
-            return 0;
-          } else {
-            return 1;
-          }
-        });
-      }
-
+      // Codemirror gives us the list of changes already sorted correctly
+      // Just handle it as it comes
       changelist.forEach(function(change) {
         var event = {
           data: change.text,
